7d71340085a550278a7ac0d56cee8b35b372d826
Mike Blackett <27811528+mikeblackett@users.noreply.github.com>
2025-05-28 13:32:43 +0100

    add support for encoding/decoding dollar-prefixed keywords

--- src/xarray_model/encoders.py
+++ src/xarray_model/encoders.py
@@ -5,13 +5,44 @@ from typing import Any, Type
 
 import numpy as np
 
+DECODE_TYPES = {
+    'string': str,
+    'integer': int,
+    'float': float,
+    'bool': bool,
+    'object': dict,
+    'array': list,
+}
+
+PYTHON_TO_JSON_KEYWORDS = {
+    'anchor': '$anchor',
+    'comment': '$comment',
+    'id': '$id',
+    'ref': '$ref',
+    'schema': '$schema',
+    'vocabulary': '$vocabulary',
+}
+
+JSON_TO_PYTHON_KEYWORDS = {
+    '$anchor': 'anchor',
+    '$comment': 'comment',
+    '$id': 'id',
+    '$ref': 'ref',
+    '$schema': 'schema',
+    '$vocabulary': 'vocabulary',
+}
+
 
 def encode_keyword(keyword: str):
-    return _snake_case_to_camel_case(keyword)
+    """Encode a Python-formatted keyword to JSON Schema."""
+    result = _snake_case_to_camel_case(keyword)
+    return PYTHON_TO_JSON_KEYWORDS.get(result, result)
 
 
 def decode_keyword(keyword: str):
-    return _camel_case_to_snake_case(keyword)
+    """Decode a JSON-Schema-formatted keyword to Python."""
+    result = _camel_case_to_snake_case(keyword)
+    return JSON_TO_PYTHON_KEYWORDS.get(result, result)
 
 
 @singledispatch
@@ -49,16 +80,6 @@ def encode_type(type_: Type) -> str:
     )
 
 
-DECODE_TYPES = {
-    'string': str,
-    'integer': int,
-    'float': float,
-    'bool': bool,
-    'object': dict,
-    'array': list,
-}
-
-
 def decode_type(type_: str) -> Type:
     return DECODE_TYPES.get(type_, type_)
 
@@ -68,5 +89,7 @@ def _camel_case_to_snake_case(string: str) -> str:
 
 
 def _snake_case_to_camel_case(string: str) -> str:
+    if '_' not in string:
+        return string
     string = ''.join(word.title() for word in string.split('_'))
     return string[0].lower() + string[1:]
