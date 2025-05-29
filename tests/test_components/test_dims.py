7f8b0e936ee4bed11ce5c50e92332f349c711ed5
Mike Blackett <27811528+mikeblackett@users.noreply.github.com>
2025-05-29 21:00:52 +0100

    improve Dims model tests

--- tests/test_components/test_dims.py
+++ tests/test_components/test_dims.py
@@ -3,16 +3,16 @@ from collections.abc import Sequence
 import hypothesis as hp
 import pytest as pt
 from hypothesis import strategies as st
-from jsonschema import ValidationError
+from jsonschema.exceptions import ValidationError
 
-from xarray_model import Shape
+from xarray_model import Dims, Name
 
 
 @st.composite
-def dims(draw, min_size=1, max_size=10):
+def dims(draw, min_size: int = 1, max_size: int | None = None):
     return draw(
-        st.iterables(
-            elements=st.text(),
+        st.lists(
+            elements=st.text(min_size=1),
             min_size=min_size,
             max_size=max_size,
         )
@@ -20,42 +20,41 @@ def dims(draw, min_size=1, max_size=10):
 
 
 class TestDims:
-    @hp.given(dims=dims())
-    def test_global_match(self, shape: Sequence[int]):
-        Shape().validate(tuple(shape))
+    @hp.given(instance=dims())
+    def test_validates_default(self, instance: Sequence[str]):
+        """Should always validate with default values."""
+        Dims().validate(tuple(instance))
 
-    @hp.given(dims=dims())
-    def test_sequence_match(self, shape: Sequence[int]):
-        Shape(shape).validate(tuple(shape))
+    @hp.given(instance=dims(), flag=st.booleans())
+    def test_validates_sequence(self, instance: Sequence[str], flag: bool):
+        """Should validate that the instance matches a sequence of names."""
+        # NOTE: Complex validation provided by `Name` is tested separately in
+        #  `test_name.py`
+        expected = [Name(name) for name in instance] if flag else instance
+        Dims(expected).validate(tuple(instance))
 
-    @hp.given(dims=dims(), data=st.data())
-    def test_sequence_match_with_wildcard(
-        self, shape: Sequence[int], data: st.DataObject
+    @hp.given(instance=dims(), expected=dims(), flag=st.booleans())
+    def test_invalidates_sequence(
+        self, instance: Sequence[str], expected: Sequence[str], flag: bool
     ):
-        expected = list(shape)
-        idx = data.draw(st.integers(min_value=0, max_value=len(expected) - 1))
-        expected[idx] = -1
-        Shape(shape).validate(tuple(shape))
+        """Should fail when the instance does not match a sequence of names."""
+        hp.assume(expected != instance)
+        expected = [Name(name) for name in expected] if flag else expected
+        with pt.raises(ValidationError):
+            Dims(expected).validate(tuple(instance))
+
+    @hp.given(instance=dims(), data=st.data())
+    def test_validates_contains(
+        self, instance: Sequence[str], data: st.DataObject
+    ):
+        """Should validate that the instance contains a name."""
+        expected = Name(data.draw(st.sampled_from(instance)))
+        Dims(contains=expected).validate(tuple(instance))
 
     @hp.given(data=st.data())
-    def test_size_match(self, data: st.DataObject):
-        min_size = data.draw(st.integers(min_value=1, max_value=2))
-        max_size = data.draw(st.integers(min_value=min_size, max_value=4))
+    def test_validates_size(self, data: st.DataObject):
+        min_size = data.draw(st.integers(min_value=1, max_value=10))
+        max_size = data.draw(st.integers(min_value=min_size, max_value=100))
         hp.assume(min_size <= max_size)
-        shape = data.draw(dims(min_size=min_size, max_size=max_size))
-        Shape(min_size=min_size, max_size=max_size).validate(tuple(shape))
-
-    @pt.mark.parametrize(
-        'expected, actual',
-        [
-            ([1, 2, 3], (1, 2)),
-            ([1, 2, 3], (1, 2, 4)),
-        ],
-    )
-    def test_sequence_mismatch(self, expected: Sequence[int], actual: tuple):
-        with pt.raises(ValidationError):
-            Shape(expected).validate(actual)
-
-    def test_size_mismatch(self):
-        with pt.raises(ValidationError):
-            Shape(min_size=1, max_size=2).validate((1, 2, 3))
+        actual = data.draw(dims(min_size=min_size, max_size=max_size))
+        Dims(min_size=min_size, max_size=max_size).validate(tuple(actual))
