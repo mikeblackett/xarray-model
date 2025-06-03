d16457a4f10185ecfebc8e50ce5d113e3c6e7e41
Mike Blackett <27811528+mikeblackett@users.noreply.github.com>
2025-06-03 16:23:12 +0100

    refactor encoders

--- src/xarray_model/encoders.py
+++ src/xarray_model/encoders.py
@@ -1,20 +1,22 @@
 from functools import singledispatch
 import re
-from collections.abc import Iterable, Mapping, Sequence
+from collections.abc import Mapping, Sequence
+from types import NoneType
 from typing import Any, Type
 
 import numpy as np
 
 DECODE_TYPES = {
-    'string': str,
-    'integer': int,
-    'float': float,
-    'bool': bool,
-    'object': dict,
     'array': list,
+    'bool': bool,
+    'float': float,
+    'integer': int,
+    'null': None,
+    'object': dict,
+    'string': str,
 }
 
-PYTHON_TO_JSON_KEYWORDS = {
+ENCODE_KEYWORDS = {
     'anchor': '$anchor',
     'comment': '$comment',
     'id': '$id',
@@ -23,7 +25,7 @@ PYTHON_TO_JSON_KEYWORDS = {
     'vocabulary': '$vocabulary',
 }
 
-JSON_TO_PYTHON_KEYWORDS = {
+DECODE_KEYWORDS = {
     '$anchor': 'anchor',
     '$comment': 'comment',
     '$id': 'id',
@@ -36,20 +38,22 @@ JSON_TO_PYTHON_KEYWORDS = {
 def encode_keyword(keyword: str):
     """Encode a Python-formatted keyword to JSON Schema."""
     result = _snake_case_to_camel_case(keyword)
-    return PYTHON_TO_JSON_KEYWORDS.get(result, result)
-
-
-def decode_keyword(keyword: str):
-    """Decode a JSON-Schema-formatted keyword to Python."""
-    result = _camel_case_to_snake_case(keyword)
-    return JSON_TO_PYTHON_KEYWORDS.get(result, result)
+    return ENCODE_KEYWORDS.get(result, result)
 
 
 @singledispatch
 def encode_value(value: Any) -> Any:
-    if isinstance(value, Sequence) and not isinstance(value, str):
-        return list(value)
     return value  # Default: identity
+
+
+@encode_value.register
+def _(value: str) -> str:
+    return value
+
+
+@encode_value.register
+def _(value: Sequence) -> list:
+    return list(value)
 
 
 @encode_value.register
@@ -62,7 +66,12 @@ def _(value: re.Pattern) -> str:
     return value.pattern
 
 
-def encode_type(type_: Type) -> str:
+@encode_value.register
+def _(value: type) -> str:
+    return _encode_type(value)
+
+
+def _encode_type(type_: Type) -> str:
     if issubclass(type_, str):
         return 'string'
     elif issubclass(type_, bool):
@@ -75,17 +84,11 @@ def encode_type(type_: Type) -> str:
         return 'object'
     elif issubclass(type_, Sequence) and not issubclass(type_, str):
         return 'array'
+    elif issubclass(type_, NoneType):
+        return 'null'
     raise TypeError(
         f'Error encoding python type {type_!r} as JSON Schema type.'
     )
-
-
-def decode_type(type_: str) -> Type:
-    return DECODE_TYPES.get(type_, type_)
-
-
-def _camel_case_to_snake_case(string: str) -> str:
-    return re.sub(r'(?<!^)(?=[A-Z])', '_', string).lower()
 
 
 def _snake_case_to_camel_case(string: str) -> str:
@@ -93,3 +96,17 @@ def _snake_case_to_camel_case(string: str) -> str:
         return string
     string = ''.join(word.title() for word in string.split('_'))
     return string[0].lower() + string[1:]
+
+
+def _camel_case_to_snake_case(string: str) -> str:
+    return re.sub(r'(?<!^)(?=[A-Z])', '_', string).lower()
+
+
+# def decode_type(type_: str) -> Type:
+#     return DECODE_TYPES.get(type_, type_)
+#
+#
+# def decode_keyword(keyword: str):
+#     """Decode a JSON-Schema-formatted keyword to Python."""
+#     result = _camel_case_to_snake_case(keyword)
+#     return DECODE_KEYWORDS.get(result, result)
